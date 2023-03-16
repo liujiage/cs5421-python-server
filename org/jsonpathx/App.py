158fe6dbc29853f085e67c2b10be34d47f609503
Joe <jiazeng1918@gmail.com>
2023-03-16 12:27:57 +0800

    update /query API

--- org/jsonpathx/App.py
+++ org/jsonpathx/App.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from helper import json_xpath
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
+
+@app.route('/query', methods=['POST'])
+def query():
+    req = request.get_json()
+    try:
+        source = req.get('source')
+        query = req.get('query')
+        return {'success': 1, 'data': {'result': json_xpath(source, query)}}
+    except:
+        return {'success': 0, 'data': {}}
+
 
 app.run(host='0.0.0.0', port=81)
---
+++ org/jsonpathx/helper.py
@@ -0,0 +1,112 @@
+import json
+import re
+
+def json_xpath(json_data, xpath):
+   
+    
+    try:
+        data = json.loads(json_data)
+    except ValueError:
+        raise ValueError("Invalid JSON")
+    
+    # Split the XPath 
+    tokens = xpath.split("/")
+    
+    for token in tokens:
+        if token == "*":
+            data = [v for k, v in data.items()] if isinstance(data, dict) else data
+        
+        #range support
+        elif "#" in token:
+            
+            path, index = token.split("#")
+            getByKey = json_xpath(json.dumps(data), path.strip())   
+            if not ":" in token:
+                index = int(index)
+                data = getByKey[index] 
+            else:
+                if not isinstance(getByKey, list):
+                    raise TypeError("range operator must work on lists")
+                start, end = index.split(":")
+                start = int(start) if start else 0
+                end = int(end) if end else len(data)
+                if end > len(data):
+                    raise IndexError("index out of bounds")
+                data = getByKey[start:end]
+            
+                
+        
+        # function support
+        elif "(" in token and ")" in token:
+            func_name, arg_expr = token.split("(")
+            arg_expr = arg_expr[:-1]
+            values = json_xpath(json.dumps(data), arg_expr.strip())    
+            
+            res = []
+            for value in values:
+                if func_name == "max":
+                    try:
+                        res.append(max(value));
+                    except ValueError:
+                        data = None
+                elif func_name == "min":
+                    try:
+                        res.append(min(value));
+                    except ValueError:
+                        data = None
+                else:
+                    raise TypeError("Invalid function name")
+            data = res                                
+
+                
+        elif "[" in token and "]" in token:
+            
+            path, remain = token.split("[")
+            filter_expr = remain[:-1]
+            withoutFilter = json_xpath(json.dumps(data), path.strip())
+            filter_match = re.match(r'(.+)(==|!=|<|>|<=|>=)(.+)', filter_expr)
+            if filter_match:
+                key = filter_match.group(1)
+                op = filter_match.group(2)
+                value = filter_match.group(3)
+                if value.isdigit():
+                    value = int(value)  
+                res = []
+                for w in withoutFilter: 
+                                  
+                    if op == '==':
+                        res.append([d for d in w if isinstance(d, dict) and d.get(key, None) == value])
+                    elif op == '!=':
+                        res.append([d for d in w if isinstance(d, dict) and d.get(key, None) != value])
+                    elif op == '<':
+                        res.append([d for d in w if isinstance(d, dict) and d.get(key, None) < value])
+                    elif op == '>':
+                        res.append([d for d in w if isinstance(d, dict) and d.get(key, None) > value])
+                    elif op == '<=':
+                        res.append([d for d in w if isinstance(d, dict) and d.get(key, None) <= value])
+                    elif op == '>=':
+                        res.append([d for d in w if isinstance(d, dict) and d.get(key, None) >= value])
+                data = res
+                                
+            else:
+                raise TypeError("wrong filter format")
+            if not data:
+                break
+                
+        
+        else:
+            if not isinstance(data, list):
+                data = [data]             
+            
+            res = []
+            for dd in data:
+                if(isinstance(dd, list)):    
+                    res.append([d.get(token, None) for d in dd if isinstance(d, dict)])
+                else:
+                    res.extend([dd.get(token, None) if isinstance(dd, dict) else None])                         
+            data = res
+                                      
+        if data is None:
+            break
+    
+    return data
