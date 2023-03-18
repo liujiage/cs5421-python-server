1e7e738e9bbe072edad4648025e2e6debbfe4287
liujiage <liujiage@yahoo.com>
2023-03-19 01:31:54 +0800

    add test case for learn jsonpath

--- org/jsonpathx/services/utils.py
+++ org/jsonpathx/services/utils.py
@@ -16,6 +16,6 @@ print out test log. Only for learn jsonpath-ng
 def print_test(file_name, search_key_word):
     movies = load_josn_by_file(file_name)
     jsonpath_expression = parse(search_key_word)
-    print(jsonpath_expression)
+    print("jsonpath_expression, type: {} value: {} ".format(type(jsonpath_expression), jsonpath_expression))
     for match in jsonpath_expression.find(movies):
-        print(match.value)
+        print("match.value: {}".format(match.value))
